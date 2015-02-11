f7c4ace475cc135a2a381e31611c69cc8e67c4a0
Klaus Laube <klaus@corp.globo.com>
2015-02-10 23:33:19 -0200

    Using some tips of seeding the math.randomseed for tests

--- tests/guessing_numbers_spec.lua
+++ tests/guessing_numbers_spec.lua
@@ -4,18 +4,20 @@ require("GuessingNumbers")
 describe('guessing the number lib', function()
 	
 	before_each(function()
-		math.randomseed(0)
+		math.randomseed(1.0)
+		math.random(); math.random(); math.random()
+
 		pick_a_number(0, 100)
 	end)
 
 	it('should pick an integer number', function ()
 		number = get_the_number()
 
-		assert.are.equals(24, number)
+		assert.are.equals(46, number)
 	end)
 
 	it('should say that the guess is higher than the picked number', function ()
-		assert.is_true(is_higher(30))
+		assert.is_true(is_higher(50))
 	end)
 
 	it('should say that the guess is lesser than the picked number', function ()
@@ -23,7 +25,7 @@ describe('guessing the number lib', function()
 	end)
 
 	it('should return true when the guessed number is equal to the picked number', function ()
-		assert.is_true(is_the_number(24))
+		assert.is_true(is_the_number(46))
 	end)
 
 end)
