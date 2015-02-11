e9946a7922cada809debe5b3cbf2aaf2da9a2688
Klaus Laube <klaus@corp.globo.com>
2015-02-10 23:53:23 -0200

    Switching from math.randomseed to a stupid Mock

--- guess-the-number/GuessingNumbers.lua
+++ guess-the-number/GuessingNumbers.lua
@@ -5,8 +5,16 @@ function seed_random()
     math.random(); math.random(); math.random()
 end
 
-function pick_a_number(lower, upper)
-    rand_number = math.random(lower, upper)
+function pick_a_number(lower, upper, describe)
+	describe = describe or false
+
+	-- FIXME: Should use math.randomseed, but this method is
+	-- not working very well in Mac OS X
+	if describe then
+		rand_number = 46
+	else
+		rand_number = math.random(lower, upper)
+	end
 end
 
 function get_the_number()
