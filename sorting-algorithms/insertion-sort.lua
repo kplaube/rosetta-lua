f5784d391c1d59fc723a578edb9160e6349e8730
Klaus Laube <klaus@corp.globo.com>
2015-03-09 23:08:23 -0300

    Using unpack to print arrays

--- sorting-algorithms/insertion-sort.lua
+++ sorting-algorithms/insertion-sort.lua
@@ -5,6 +5,4 @@ local sorting = require('lib/sorting_lib')
 local myarray = {10, -1, -2, 5, 8, 9}
 
 sorting:insertion_sort(myarray)
-for i in next, myarray do
-    print(myarray[i])
-end
+print(unpack(myarray))
