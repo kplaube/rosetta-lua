edca366fbe524766fb779ec8ce5b8a5f60226ecc
Klaus Laube <klaus@corp.globo.com>
2016-03-02 21:02:44 -0300

    Optimizing GCD with Euclidean algorithm

--- lib/greatest_common_divisor.lua
+++ lib/greatest_common_divisor.lua
@@ -1,13 +1,21 @@
-local function gcd(a, b)
-    local best = 0
+--[[local function gcd(a, b)]]
+    --local best = 0
 
-    for i=0, (a + b), 1 do
-        if (a % i) == 0 and (b % i) == 0 then
-            best = i
-        end
+    --for i=0, (a + b), 1 do
+        --if (a % i) == 0 and (b % i) == 0 then
+            --best = i
+        --end
+    --end
+
+    --return best
+--end
+
+local function euclideanGcd(a, b)
+    if b == 0 then
+        return a
     end
 
-    return best
+    return euclideanGcd(b, (a % b))
 end
 
-return gcd
+return euclideanGcd
