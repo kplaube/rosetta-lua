8854b8a06a7ad6cc6b688ae6633df782c9255dde
Klaus Laube <kplaube@gmail.com>
2017-02-02 10:27:19 -0200

    Fix 99 Bottles

--- rosetta/99-bottles-of-beer.lua
+++ rosetta/99-bottles-of-beer.lua
@@ -1,8 +1,16 @@
 -- http://rosettacode.org/wiki/99_Bottles_of_Beer
 
+local function sayBottle(i)
+    if i > 1 then
+        return "bottles"
+    end
+
+    return "bottle"
+end
+
 for i=100, 1, -1 do
-    print(i .. " bottles of beer on the wall")
-    print(i .. " bottles of beer")
+    print(i .. " " .. sayBottle(i) .. " of beer on the wall")
+    print(i .. " " .. sayBottle(i) .. " of beer")
     print("Take one down, pass it around")
-    print(i - 1 .. " bottles of beer on the wall\n")
+    print(i - 1 .. " " .. sayBottle(i - 1) .. " of beer on the wall\n")
 end
